b9901cc98960d27400df54b9ef49d36c7b270320
hi-tech <hi-tech@DESKTOP-C6O8FHU>
2024-01-20 19:53:57 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 1.Вывести на экран в столбик первые 10 натуральных чисел(циклы с условием, цикл с параметром)
+            //с условием:
+            //int x = 1;
+            //while (x<=10)
+            //{
+            //    Console.WriteLine(x);
+            //    x++;
+            //}
+
+            //c параметром:
+            //for (int x = 1; x <=10; x++)
+            //{
+            //    Console.WriteLine(x);
+            //}
+
+
+            //Задание 2.Вывести на экран целые числа из интервала от 1 до n(цикл с параметром)
+            //Console.WriteLine("Введите число: ");
+            //int n = int.Parse(Console.ReadLine());
+            //for (int x = 1; x <= n; x++)
+            //{
+            //    Console.WriteLine(x);
+            //}
+
+            //Задание 3.Вывести в столбик 5 раз слово «Привет!» (циклы с условием)
+            //int x = 1;
+            //while (x<=5)
+            //{
+            //    Console.WriteLine("ПРИВЕТ!");
+            //    x++;
+            //}
+
+            //Задание 4.С использованием цветового оформления консоли:
+            //а) вывести на экран горизонтальную строку из 18 символов;
+            //Console.ForegroundColor = ConsoleColor.Magenta;
+            //for (int i = 1; i <= 18; i++)
+            //{
+            //    Console.Write('.');
+            //}
+            //б) вывести на экран вертикальную строку из 25 символов
+            //Console.ForegroundColor = ConsoleColor.Blue;
+            //for (int i = 1; i <=25; i++)
+            //{
+            //    Console.WriteLine('.');
+            //}
+            //Console.ReadKey();
+
+        }
+    }
+}
